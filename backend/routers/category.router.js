64dc1d8acc893b46fa6f0254a2ed0d2dc5a59d38
Muhammet Tokuş <93308444+Mtokus@users.noreply.github.com>
2023-08-24 01:27:51 +0300

    .

--- backend/routers/category.router.js
+++ backend/routers/category.router.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../models/category");
-const { v4:uuidv4 } = require("uuid");
-
+const { v4: uuidv4 } = require("uuid");
 
 router.post("/add", async (req, res) => {
   try {
-    const {name}=req.body;
-    const category = new Category({
-      _id: uuidv4(),
-      name: name,
-    });
+    const { name } = req.body;
 
+    const checkName = await Category.findOne({ name: name });
+    if (checkName != null) {
+      res.status(403).json({ message: "Bu kategori daha önce eklendi" });
+    } else {
+      const category = new Category({
+        _id: uuidv4(),
+        name: name,
+      });
 
-    await category.save();
+      await category.save();
 
-    res.json({ message: "Kategory Kaydı Başarıyla Tamamlandı " });
+      res.json({ message: "Kategory Kaydı Başarıyla Tamamlandı " });
+    }
   } catch (error) {
     res.stats(500).json({ message: error.message });
   }
@@ -35,7 +39,7 @@ router.post("/removeById", async (req, res) => {
 router.post("/update", async (req, res) => {
   try {
     const { _id, name } = req.body;
-    const category = await Category.findOne({ _id:_id });
+    const category = await Category.findOne({ _id: _id });
     category.name = name;
     await Category.findByIdAndUpdate(_id, category);
 
